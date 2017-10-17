9908c12aea485ac04669c36f2820f7512e64111a
Tamunobelema Membere <P14186800@LEC-ADMIN.dmu.ac.uk>
2017-10-17 15:44:39 +0100

    Changed README.md again

--- js/greeting-nocomments.js
+++ js/greeting-nocomments.js
@@ -2,9 +2,14 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  //let myHeader = document.getElementById("my-header");
+  //ADD ANOTHER GETELEMENTBYID
+	
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
+	//myHeader.innerHTML = userName.value;
+	//COPY LINE ABOVE BUT USE THE VARIABLE HOLDING YOUR H2 ID
     event.preventDefault();
     return false;
   }
